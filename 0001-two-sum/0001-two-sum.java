d2806969e142c7b529ecaaf1e8bd3f4684bdd81c
Jefferson Barroso Freitas <98049603+jefferson-barroso@users.noreply.github.com>
2025-02-18 09:43:29 -0300

    Time: 2 ms (98.89%), Space: 45.1 MB (44.52%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complemento = target - nums[i];
+            
+            if (numMap.containsKey(complemento)) {
+                return new int[]{numMap.get(complemento), i};
+            }
+            
+            numMap.put(nums[i], i);
+        }
+        
+        throw new IllegalArgumentException("Nenhuma solução encontrada");
+    }
+}
